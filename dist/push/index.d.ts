0f64c1d4ecd7831a6f4cf08677af3ac4704f522e
Justin Kambic <jk@elastic.co>
2023-05-03 23:47:44 -0400

    Dist fixup.

--- dist/push/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Monitor } from '../dsl/monitor';
-import type { PushOptions, ProjectSettings } from '../common_types';
-export declare function push(monitors: Monitor[], options: PushOptions): Promise<void>;
-export declare function formatDuplicateError(monitors: Set<Monitor>): string;
-export declare function loadSettings(): Promise<ProjectSettings>;
-export declare function validateSettings(opts: PushOptions): void;
-export declare function catchIncorrectSettings(settings: ProjectSettings, options: PushOptions): Promise<void>;
-export declare function pushLegacy(monitors: Monitor[], options: PushOptions, version: number): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
